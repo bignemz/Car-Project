ac3caa111943abf9916ee52ec45e26b623d4c8ef
bignemz <chinaemeemerole@yahoo.com>
2023-06-24 11:57:54 -0700

    my

--- act/src/App.js
+++ act/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes,Route ,Link } from 'react-router-dom';
 
 import About from './about';
 import Home from './Home';
+import Car from './Car';
 
 export default function App(){
   return(
@@ -15,12 +16,15 @@ export default function App(){
 
           <Link className='link'to="/about">About</Link>
 
+          <Link className='link'to="/car">Car</Link>
+
         </div>
       </nav>
     <Routes>
    
       <Route path='/' element={<Home/>}/>
       <Route path='/about' element={<About/>}/>
+       <Route path='/car' element={<Car/>}/>
     </Routes>
    
     </BrowserRouter>
